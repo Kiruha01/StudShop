6f072066de812868c9c29f0758bbbca19d22b0ff
Kiruha01 <liss-2001g@mail.ru>
2021-10-25 17:38:36 +0300

    Init database

--- web/backend/main.py
+++ web/backend/main.py
@@ -1,14 +1,21 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from .user import users
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.register_blueprint(users, url_prefix='/api/user/')
+    app.config.from_object('config.Config')
 
-    return app
+    db.init_app(app)
+    with app.app_context():
+        app.register_blueprint(users, url_prefix='/api/user/')
+        db.create_all()  # Create sql tables for our data models
+
+        return app
